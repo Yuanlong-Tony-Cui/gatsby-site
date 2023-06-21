8daf812c7b13cfdc4f9bf63f31be4f23c6e0b168
Yuanlong Cui <yuanlongcui@v1040-wn-rt-a-47-106.campus-dynamic.uwaterloo.ca>
2023-06-20 20:36:44 -0400

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
