dcfb7b0476f577915531df15481fdd4834ff2423
Yuanlong Cui <yuanlongcui@v1040-wn-rt-a-95-68.campus-dynamic.uwaterloo.ca>
2023-06-22 20:41:53 -0400

    Finished Gatsby Tutorial Part 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>I'm the creator of this site.</p>
     </Layout>
   )
 }
 
-export const Head = () => <title>About Me</title>
+export const Head = () => <Seo title="About Me" />
 
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
 
 // const pageStyles = {
 //   color: "#232129",
@@ -176,7 +177,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>I'm building this site by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
@@ -185,6 +186,6 @@ const IndexPage = () => {
   )
 }
 
-export default IndexPage
+export const Head = () => <Seo title="Home Page" />
 
-export const Head = () => <title>Home Page</title>
+export default IndexPage
