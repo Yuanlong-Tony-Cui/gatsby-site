cc2d9dbeac82ac55a36b931bbb02aae421a528e2
Anthony Cui <yuanlongcui0511@gmail.com>
2025-04-06 17:32:58 -0400

    Add tags for Career posts

--- src/pages/career/index.js
+++ src/pages/career/index.js
@@ -6,21 +6,25 @@ import {
   postLinkText,
   postDesc,
   heroImage,
+  tagBar,
+  tagPill,
 } from '../main.module.css'
 import { StaticImage } from 'gatsby-plugin-image'
 
 const BlogPage = ({ data }) => {
-    return (
-      <Layout pageTitle="Posts: Career">
-        <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-          <StaticImage
-            alt="Career Cover"
-            src="./uoft-front-campus.jpeg"
-            className={heroImage}
-          />
-        </div>
-        { // Loop through the data and display the blog posts:
-          data.allMdx.nodes.map((node) => (
+  return (
+    <Layout pageTitle="Posts: Career">
+      <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+        <StaticImage
+          alt="Career Cover"
+          src="./uoft-front-campus.jpeg"
+          className={heroImage}
+        />
+      </div>
+      { // Loop through the data and display the blog posts:
+        data.allMdx.nodes.map((node) => {
+          const tags = node.frontmatter.tags?.split(",").map(item => item.trim());
+          return (
             <article key={node.id}>
               <h2>
                 <Link to={`/career/${node.frontmatter.slug}`} className={postLinkText}>
@@ -28,11 +32,18 @@ const BlogPage = ({ data }) => {
                 </Link>
               </h2>
               <p className={postDesc}>{node.frontmatter.date}</p>
+              <div className={tagBar}>
+                { tags && tags.map(tag => (
+                    <div className={tagPill}>{tag}</div>
+                  ))
+                }
+              </div>
             </article>
-          ))
-        }
-      </Layout>
-    )
+          )
+        })
+      }
+    </Layout>
+  )
 }
 
 export const query = graphql`
@@ -46,6 +57,7 @@ export const query = graphql`
           date(formatString: "MMMM D, YYYY")
           title
           slug
+          tags
         }
         id
       }
